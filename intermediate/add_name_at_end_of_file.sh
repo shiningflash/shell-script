03683ff68ad891060f0afdb2659e66f0a5c06383
amirulislamalmamun@gmail.com <amirulislamalmamun@gmail.com>
2021-07-03 22:32:46 +0600

    intermidiate level work updated

--- intermediate/add_name_at_end_of_file.sh
+++ intermediate/add_name_at_end_of_file.sh
@@ -1,9 +1,16 @@
 #! /bin/bash
 
-FILES=$(ls *.txt)
-NAME="MY NAME HERE"
+read -p "Enter a number: " ID
 
-for FILE in $FILES
-    do
-        echo -e "\n$NAME" >> $FILE
-done
+YEAR=$(echo $ID | cut -c1-2)
+SEMESTER=$(($(echo $ID | cut -c3)-1))
+DEPT=$(echo $ID | cut -c4-5)
+
+SEMESTER_NAMES=(Spring Fall Summer)
+
+if [ $DEPT == "01" ]
+then
+    echo "The student is from Dept. of CSE enrolled in ${SEMESTER_NAMES[$SEMESTER]} 20$YEAR"
+else
+    echo "The student is not from Dept. of CSE"
+fi 
